c8dd91b374b08fe75aa2841ca31a7fbc4360a867
DevSharif <sharif.hashmi02@gmail.com>
2025-11-15 12:49:46 +0530

    change base url

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/engfrom/';
+$config['base_url'] = 'http://localhost/git-projects/EnglishFrom';
 // $config['base_url'] = 'https://englishfrom.com/';
 
 /*
